270607c49a235bdb697de887f4a298694159dd19
Jefferson Fausto Vaz <faustovaz@gmail.com>
2021-08-27 18:23:14 -0300

    Added new user

--- src/main/resources/db/migration/V7__create_usuarios.sql
+++ src/main/resources/db/migration/V7__create_usuarios.sql
@@ -5,3 +5,5 @@ create table usuarios(
 
 insert into usuarios(username, password) values ('michele', '$2y$04$0eejWDFTb5Fp9l7u0GvXfOmOlP3jc6hyBWUdVOBKpzO48yXzs1EGy');
 insert into usuarios(username, password) values ('cida', '$2y$04$0eejWDFTb5Fp9l7u0GvXfOmOlP3jc6hyBWUdVOBKpzO48yXzs1EGy');
+insert into usuarios(username, password) values ('fausto', '$2y$04$L6s2gi0V7jfj/zLDi6MxoOD7MG4br0Mglu6bTYk9UToHa40GtVnra');
+
