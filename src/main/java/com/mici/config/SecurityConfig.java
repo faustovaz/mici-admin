b8853517baf359480fe68c9577fd2ac44c6e051e
Jefferson Fausto Vaz <faustovaz@gmail.com>
2021-08-30 15:54:47 -0300

    Added Mici logo

--- src/main/java/com/mici/config/SecurityConfig.java
+++ src/main/java/com/mici/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().antMatchers("/webjars/**").permitAll()
+			.authorizeRequests().antMatchers("/webjars/**", "/img/**").permitAll()
 			.and()
 				.formLogin().loginPage("/login").permitAll()
 			.and()
