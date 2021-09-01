e48b169133cd3e0ea0e479fd757b7d0c809c4e28
Jefferson Fausto Vaz <faustovaz@gmail.com>
2021-09-01 07:51:52 -0300

    Added new css

--- src/main/java/com/mici/config/SecurityConfig.java
+++ src/main/java/com/mici/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().antMatchers("/webjars/**", "/img/**", "/favicon.ico").permitAll()
+			.authorizeRequests().antMatchers("/webjars/**", "/img/**", "/favicon.ico", "/css/**").permitAll()
 			.and()
 				.formLogin().loginPage("/login").permitAll()
 			.and()
